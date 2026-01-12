fdefd052b235484090020ee05286117e884369aa
Geek Protocol <geek.coin.protocol@gmail.com>
2026-01-12 21:59:42 +0000

    feat: show landing content on /dashboard

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,5 +1,32 @@
-import { redirect } from "next/navigation";
+"use client";
 
-export default function Page() {
-  redirect("/");
+import { LandingFooter } from "@/components/LandingFooter";
+import { LandingHero } from "@/components/LandingHero";
+import {
+  StorySection,
+  CategoriesSection,
+  RoadmapSection,
+  KaspaHonorSection,
+  CTASection,
+  NavLinks,
+  ImpactSection,
+  ResourcesSection,
+} from "@/components/LandingPage";
+
+export default function DashboardLanding() {
+  return (
+    <main className="w-full bg-black text-white">
+      {/* Reuse the full landing experience on /dashboard */}
+      <LandingHero />
+      <StorySection />
+      <ImpactSection />
+      <CategoriesSection />
+      <RoadmapSection />
+      <KaspaHonorSection />
+      <ResourcesSection />
+      <NavLinks />
+      <CTASection />
+      <LandingFooter />
+    </main>
+  );
 }
